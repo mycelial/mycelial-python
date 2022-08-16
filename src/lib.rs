596598f030c175a9f0a25d25ca39a51a1520f023
Dmitry Mazurin <dimas.m.19@gmail.com>
2022-08-16 23:13:59 +0100

    fmt & clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 use mycelial_crdt::{list, vclock};
+use pyo3::exceptions;
 use pyo3::prelude::*;
 use pyo3::types;
-use pyo3::exceptions;
-use serde_json;
 
 #[derive(Debug)]
 #[pyclass]
 pub struct List(list::List);
-
 
 #[derive(Debug)]
 #[repr(transparent)]
@@ -15,19 +13,18 @@ pub struct WrappedValue<Key>(list::Value<Key>);
 
 impl<'a, Key> From<&'a list::Value<Key>> for &'a WrappedValue<Key> {
     fn from(v: &'a list::Value<Key>) -> Self {
-        unsafe{ &*(v as *const list::Value<Key> as *const WrappedValue<Key>) }
+        unsafe { &*(v as *const list::Value<Key> as *const WrappedValue<Key>) }
     }
 }
 
-
+#[allow(clippy::from_over_into)]
 impl<Key> Into<list::Value<Key>> for WrappedValue<Key> {
     fn into(self) -> list::Value<Key> {
         self.0
     }
 }
 
-impl<Key> ToPyObject for WrappedValue<Key>
-{
+impl<Key> ToPyObject for WrappedValue<Key> {
     fn to_object(&self, py: Python<'_>) -> PyObject {
         match self.0 {
             list::Value::Bool(b) => b.to_object(py),
@@ -43,27 +40,29 @@ impl<Key> ToPyObject for WrappedValue<Key>
                 .to_object(py),
             _ => unreachable!(),
         }
-        .into()
     }
 }
 
 impl<'source, Key> FromPyObject<'source> for WrappedValue<Key> {
     fn extract(obj: &'source types::PyAny) -> PyResult<Self> {
         if let Ok(b) = obj.downcast::<types::PyBool>() {
-            return Ok(WrappedValue(list::Value::Bool(b.is_true())))
+            return Ok(WrappedValue(list::Value::Bool(b.is_true())));
         }
         if let Ok(f) = obj.downcast::<types::PyFloat>() {
-            return Ok(WrappedValue(list::Value::Float(f.value() as f64)))
+            return Ok(WrappedValue(list::Value::Float(f.value() as f64)));
         }
         if let Ok(s) = obj.downcast::<types::PyString>() {
-            return Ok(WrappedValue(list::Value::Str(s.to_str()?.into())))
+            return Ok(WrappedValue(list::Value::Str(s.to_str()?.into())));
         }
         if let Ok(l) = obj.downcast::<types::PyList>() {
-            let vec = l.iter().map(|v| {
-                let v: PyResult<WrappedValue<Key>> = v.extract();
-                v.map(|v| v.into())
-            }).collect::<PyResult<Vec<list::Value<Key>>>>()?;
-            return Ok(WrappedValue(list::Value::Vec(vec)))
+            let vec = l
+                .iter()
+                .map(|v| {
+                    let v: PyResult<WrappedValue<Key>> = v.extract();
+                    v.map(|v| v.into())
+                })
+                .collect::<PyResult<Vec<list::Value<Key>>>>()?;
+            return Ok(WrappedValue(list::Value::Vec(vec)));
         }
         Err(exceptions::PyValueError::new_err("unsupported value"))
     }
@@ -106,12 +105,12 @@ impl List {
 
     fn diff(&self, py: Python<'_>, vclock: PyObject) -> PyResult<PyObject> {
         if let Ok(encoded) = vclock.cast_as::<types::PyString>(py) {
-            let vc: vclock::VClock = serde_json::from_str(&encoded.to_string()).map_err(to_error)?;
+            let vc: vclock::VClock =
+                serde_json::from_str(&encoded.to_string()).map_err(to_error)?;
             let diff = serde_json::to_string(&self.0.diff(&vc)).map_err(to_error)?;
-            return Ok(diff.to_object(py))
+            return Ok(diff.to_object(py));
         }
         Err(exceptions::PyValueError::new_err("bad vclock"))
-
     }
 
     fn to_vec(&self, py: Python<'_>) -> PyResult<PyObject> {
